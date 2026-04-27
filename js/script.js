4496f7b045b8d688ee3d9038dd8eaec4c07d087f
Gabriellehub <gauthiergabrielle2007@gmail.com>
2026-04-27 13:10:16 -0400

    debut javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const boutonmodale = document.querySelectorAll(".bouton");
+const modale = document.querySelector(".modale")
+
+for(var i=0;i<boutonmodale.length;i++){
+    boutonmodale[i].addEventListener("click",openmodale);
+}
+
+function openmodale(){
+
+}
